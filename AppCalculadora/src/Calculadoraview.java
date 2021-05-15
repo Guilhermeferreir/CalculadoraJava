5c6f70786190adb106111efc5f5f29e3307792f0
Guilherme Ferreira <83267524+Guilhermeferreir@users.noreply.github.com>
2021-05-15 04:08:22 -0300

    Update Calculadoraview.java

--- AppCalculadora/src/Calculadoraview.java
+++ AppCalculadora/src/Calculadoraview.java
@@ -308,7 +308,7 @@ public class Calculadoraview extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // Botton =
+        // Button =
         valor2 = Double.parseDouble(txtresultado.getText());
         if(sinal == "somar"){
             txtresultado.setText(String.valueOf(valor1+valor2));
@@ -326,13 +326,13 @@ public class Calculadoraview extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton12ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton12ActionPerformed
-        // Botton 0
+        // Button 0
         txtresultado.setText(txtresultado.getText()+ "0");
        
     }//GEN-LAST:event_jButton12ActionPerformed
 
     private void jButton10ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton10ActionPerformed
-        // Botton .
+        // Button .
         txtresultado.setText(txtresultado.getText()+ ".");
     }//GEN-LAST:event_jButton10ActionPerformed
 
@@ -341,79 +341,79 @@ public class Calculadoraview extends javax.swing.JFrame {
     }//GEN-LAST:event_txtresultadoActionPerformed
 
     private void jButton18ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton18ActionPerformed
-        // Botton clear 
+        // Button clear 
         txtresultado.setText("");
     }//GEN-LAST:event_jButton18ActionPerformed
 
     private void jButton14ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton14ActionPerformed
-        // Botton 1
+        // Button 1
         txtresultado.setText(txtresultado.getText()+ "1");
     }//GEN-LAST:event_jButton14ActionPerformed
 
     private void jButton13ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton13ActionPerformed
-        // Botton 2
+        // Button 2
         txtresultado.setText(txtresultado.getText()+ "2");
     }//GEN-LAST:event_jButton13ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        //Botton 3
+        //Button 3
         txtresultado.setText(txtresultado.getText()+ "3");
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // Botton 4
+        // Button 4
         txtresultado.setText(txtresultado.getText()+ "4");
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
-        // botton 5
+        // Button 5
         txtresultado.setText(txtresultado.getText()+ "5");
     }//GEN-LAST:event_jButton7ActionPerformed
 
     private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
-        //Botton 6
+        //Button 6
         txtresultado.setText(txtresultado.getText()+ "6");
         
     }//GEN-LAST:event_jButton8ActionPerformed
 
     private void jButton15ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton15ActionPerformed
-        // Botton 7
+        // Button 7
         txtresultado.setText(txtresultado.getText()+ "7");
     }//GEN-LAST:event_jButton15ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // Botton 8
+        // Button 8
         txtresultado.setText(txtresultado.getText()+ "8");
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton9ActionPerformed
-        // Botton 9
+        // Button 9
         txtresultado.setText(txtresultado.getText()+"9");
     }//GEN-LAST:event_jButton9ActionPerformed
 
     private void jButton11ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton11ActionPerformed
-        // Botton plus
+        // Button plus
         valor1 = Double.parseDouble(txtresultado.getText());
         txtresultado.setText("");
         sinal = "somar";
     }//GEN-LAST:event_jButton11ActionPerformed
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
-        // Botton less
+        // Button less
         valor1 = Double.parseDouble(txtresultado.getText());
         txtresultado.setText("");
         sinal = "subtrair";
     }//GEN-LAST:event_jButton6ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-        // Botton share
+        // Button share
         valor1 = Double.parseDouble(txtresultado.getText());
         txtresultado.setText("");
         sinal = "dividir";
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton16ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton16ActionPerformed
-        // Botton multiply
+        // Button multiply
          valor1 = Double.parseDouble(txtresultado.getText());
         txtresultado.setText("");
         sinal = "multiplicar";
@@ -421,7 +421,7 @@ public class Calculadoraview extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton16ActionPerformed
 
     private void jButton17ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton17ActionPerformed
-        // Botton ON
+        // Button ON
         txtresultado.setText("");
     }//GEN-LAST:event_jButton17ActionPerformed
 
